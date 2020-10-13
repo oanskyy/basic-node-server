09e0cd5ce9b27f33c6d51e788fbc12a1b08d7376
codessbabe <oana.anghelachi@gmail.com>
2020-10-13 15:41:37 +0300

    Create basic node server

---
+++ test.js
@@ -0,0 +1,14 @@
+let http = require("http")
+
+let ourApp = http.createServer(function(req, res) { 
+  if(req.url == "/") { 
+    res.end("Hello, and welcome to our website.")
+  }
+  
+  if(req.url == "/about"){ 
+    res.end("About page")
+  }
+
+  res.end("we cannot find the page")
+})
+ourApp.listen(3000)
